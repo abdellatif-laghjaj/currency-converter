1784560b9d72fcd68ff8f9c2d2a81aa97c5a72e9
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-05 21:49:14 +0100

    add button

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const amount_input = document.querySelector('.amount-input');
+const from_currency = document.querySelector('.from-dropdown');
+const to_currency = document.querySelector('.to-dropdown');
+const input_value = document.querySelector('.input-value');
+const output_value = document.querySelector('.output-value');
+const input_currency = document.querySelector('.input-currency');
+const output_currency = document.querySelector('.output-currency');
