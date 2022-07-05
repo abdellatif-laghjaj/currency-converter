4f7c83ddd5d612743a2a4c5e38825dc0d99ce440
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-05 23:42:12 +0100

    show result

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@ const convert_btn = document.querySelector('.convert-btn');
 
 
 //api url
-const api_url = 'https://open.er-api.com/v6/latest/';
+// const api_url = 'https://open.er-api.com/v6/latest/';
+const API_KEY = '3237605e72ae640493e2c0bd';
+const api_url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/`;
 
 //values
 var from_value = '';
@@ -23,11 +25,43 @@ convert_btn.addEventListener('click', () => {
     if (amount == '' || from_value == '' || to_value == '') {
         alert('Please enter an amount');
     } else {
-        alert('Converting...');
+        displayConversion(from_value, to_value, amount);
     }
 });
 
 
+//fucntion to get the currency rates
+function getCurrencyRates(from, to) {
+    return fetch(`${api_url}${from}`)
+        .then(response => response.json())
+        .then(data => {
+            return data.conversion_rates[to];
+        });
+}
+
+console.log(getCurrencyRates('USD', 'INR'));
+
+//calculate the conversion
+function calculateConversion(from, to, amount) {
+    return getCurrencyRates(from, to)
+        .then(rate => {
+            return amount * rate;
+        });
+}
+
+console.log(calculateConversion('USD', 'MAD', 100));
+
+
+//result of the conversion
+function displayConversion(from, to, amount) {
+    input_currency.innerHTML = amount;
+    output_currency.innerHTML = calculateConversion(from, to, amount);
+    from_currency.value = from_value;
+    to_currency.value = to_value;
+}
+
+
+//get the currency rates
 for ([key, value] of Object.entries(CURRENCY_LIST)) {
     from_currency.innerHTML += `<option value="${value}">${key}</option>`;
     to_currency.innerHTML += `<option value="${value}">${key}</option>`;
